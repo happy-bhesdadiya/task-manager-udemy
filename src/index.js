5534c3c3ed97b03810370fe147db724c6db90eb9
Happy Bhesdadiya <happy.bhesdadiya@bacancy.com>
2021-03-05 14:23:11 +0530

    Added tests

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-const express = require('express');
-require('./db/mongoose')
+const app = require('./app');
+
+// const express = require('express');
+// require('./db/mongoose')
 // const User = require('./models/user')
 // const Task = require('./models/task');
-const { Router } = require('express');
-const multer = require('multer');
+// const { Router } = require('express');
+// const multer = require('multer');
 
-const userRouter = require('./routers/user')
-const taskRouter = require('./routers/task')
+// const userRouter = require('./routers/user')
+// const taskRouter = require('./routers/task')
 
-const app = express()
+// const app = express()
 const port = process.env.PORT
 
 // app.use((req, res, next) => {
@@ -23,32 +25,32 @@ const port = process.env.PORT
 //     res.status(503).send('Site is under maintainance')
 // })
 
-const upload = multer({
-    dest: 'images',
-    limits: {
-        fileSize: 1000000
-    },
-    fileFilter(req, file, cb) {
-        if (!file.originalname.match(/\.(doc|docx)$/)) {
-            return cb(new Error('Please upload document'))
-        }
-        cb(undefined, true)
-    }
-})
+// const upload = multer({
+//     dest: 'images',
+//     limits: {
+//         fileSize: 1000000
+//     },
+//     fileFilter(req, file, cb) {
+//         if (!file.originalname.match(/\.(doc|docx)$/)) {
+//             return cb(new Error('Please upload document'))
+//         }
+//         cb(undefined, true)
+//     }
+// })
 
 // const errorMiddleware = (req, res, next) => {
 //     throw new Error('From my middleware')
 // }
 
-app.post('/upload', upload.single('upload'), (req, res) => {
-    res.send()
-}, (error, req, res, next) => {
-    res.status(400).send({ error: error.message })
-})
+// app.post('/upload', upload.single('upload'), (req, res) => {
+//     res.send()
+// }, (error, req, res, next) => {
+//     res.status(400).send({ error: error.message })
+// })
 
-app.use(express.json())
-app.use(userRouter)
-app.use(taskRouter)
+// app.use(express.json())
+// app.use(userRouter)
+// app.use(taskRouter)
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port);
@@ -81,18 +83,17 @@ app.listen(port, () => {
 
 // Video 114 ...
 
-const Task = require('./models/task')
-const User = require('./models/user')
+// const Task = require('./models/task')
+// const User = require('./models/user')
 
-const main = async () => {
-    // const task = await Task.findById('5fdcb45c3f82ab3044105ea0')
-    // await task.populate('owner').execPopulate()
-    // console.log(task.owner);
+// const main = async () => {
+//     const task = await Task.findById('5fdcb45c3f82ab3044105ea0')
+//     await task.populate('owner').execPopulate()
+//     console.log(task.owner);
 
-    // const user = await User.findById('5fdcb40fdfb0f936bcadda04')
-    // await user.populate('tasks').execPopulate()
-    // console.log(user.tasks);
+//     const user = await User.findById('5fdcb40fdfb0f936bcadda04')
+//     await user.populate('tasks').execPopulate()
+//     console.log(user.tasks);
+// }
 
-}
-
-main()
+// main()
